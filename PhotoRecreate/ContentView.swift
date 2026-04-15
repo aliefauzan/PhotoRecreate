5ad36bbc3165982c81819b4d117e19cecc782784
Antonio Palomba <antonio.palomba2015@gmail.com>
2026-04-15 09:10:53 +0800

    Initial Commit

---
+++ PhotoRecreate/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  PhotoRecreate
+//
+//  Created by Antonio Palomba on 15/04/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
