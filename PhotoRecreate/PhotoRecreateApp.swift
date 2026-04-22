56e2a3f5a1c1f2ab41a2e30e02d39e28dca7672c
aliefauzan <afindo.mi01@gmail.com>
2026-04-22 08:57:57 +0800

    feat: implement main application structure for PhotoRecreate

---
+++ PhotoRecreate/PhotoRecreateApp.swift
@@ -0,0 +1,17 @@
+//
+//  PhotoRecreateApp.swift
+//  PhotoRecreate
+//
+//  Created by Andi Muhammad Alief Fauzan on 22/04/26.
+//
+
+import SwiftUI
+
+@main
+struct PhotoRecreateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView() 
+        }
+    }
+}
